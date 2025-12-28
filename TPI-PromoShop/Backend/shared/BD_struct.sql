5ec2b508f33916a7bb5d4b8414ada384221ab9da
Mauro-Dorigoni <dorigonimauro@gmail.com>
2025-12-28 12:24:09 -0300

    Updated sql create schema

--- TPI-PromoShop/Backend/shared/BD_struct.sql
+++ TPI-PromoShop/Backend/shared/BD_struct.sql
@@ -1,95 +1,221 @@
-drop database promoShop;
-Create database promoShop;
-Use promoShop;
+CREATE DATABASE  IF NOT EXISTS `promoshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `promoshop`;
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: promoshop
+-- ------------------------------------------------------
+-- Server version	8.0.41
 
-create table `userCategory`(
-	id int unsigned auto_increment primary key,
-    categoryType varchar(255) not null unique,
-    dateDeleted date default null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table `shopType`(
-	id int unsigned auto_increment primary key,
-    `type` varchar(255) unique,
-    `description` text,
-    dateDeleted date default null
-);
+--
+-- Table structure for table `news`
+--
 
-create table `user`(
-	`id` int unsigned auto_increment primary key,
-    `email` varchar(255) NOT NULL unique,
-    `pass` varchar(255) not null,
-    `isAdmin` boolean default null,
-    `isOwner` boolean default null,
-    dateDeleted date default null,
-    emailToken varchar(255) default null unique,
-    isEmailVerified boolean,
-    idUserCategory int unsigned,
-    foreign key (idUserCategory) references userCategory (id) on update cascade
-);
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `news` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `newsText` text NOT NULL,
+  `dateFrom` date NOT NULL,
+  `dateTo` date NOT NULL,
+  `dateDeleted` date DEFAULT NULL,
+  `idAdmin` int unsigned DEFAULT NULL,
+  `idUserCategory` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idAdmin` (`idAdmin`),
+  KEY `idUserCategory` (`idUserCategory`),
+  CONSTRAINT `news_ibfk_1` FOREIGN KEY (`idAdmin`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `news_ibfk_2` FOREIGN KEY (`idUserCategory`) REFERENCES `usercategory` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table shop (
-	id int unsigned auto_increment primary key,
-    `name` varchar(255) not null unique,
-    location varchar(255) not null,
-    dateDeleted date default null,
-    idOwner int unsigned,
-    idShopType int unsigned,
-    foreign key (idOwner) references `user`(id) on update cascade,
-    foreign key (idShopType) references shopType (id) on update cascade
-);
+--
+-- Table structure for table `promotion`
+--
 
-create table news(
-	id int unsigned auto_increment primary key,
-    newsText text not null,
-    dateFrom date not null,
-    dateTo date not null,
-    dateDeleted date default null,
-    idAdmin int unsigned,
-    idUserCategory int unsigned, 
-    foreign key (idAdmin) references `user` (id) on update cascade,
-    foreign key (idUserCategory) references userCategory (id) on update cascade
-);
+DROP TABLE IF EXISTS `promotion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promotion` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `promoText` text NOT NULL,
+  `dateFrom` date NOT NULL,
+  `dateTo` date NOT NULL,
+  `dateDeleted` date DEFAULT NULL,
+  `imageUUID` varchar(255) DEFAULT NULL,
+  `idShop` int unsigned DEFAULT NULL,
+  `idUserCategory` int unsigned DEFAULT NULL,
+  `idAdmin` int unsigned DEFAULT NULL,
+  `status` varchar(9) DEFAULT NULL,
+  `motivoRechazo` text,
+  PRIMARY KEY (`id`),
+  KEY `idShop` (`idShop`),
+  KEY `idUserCategory` (`idUserCategory`),
+  KEY `idAdmin` (`idAdmin`),
+  CONSTRAINT `promotion_ibfk_1` FOREIGN KEY (`idShop`) REFERENCES `shop` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `promotion_ibfk_2` FOREIGN KEY (`idUserCategory`) REFERENCES `usercategory` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `promotion_ibfk_3` FOREIGN KEY (`idAdmin`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table promotion (
-	id int unsigned auto_increment primary key,
-    promoText text not null,
-    dateFrom date not null,
-    dateTo date not null,
-    isApproved boolean default null,
-    dateDeleted date default null,
-    imageUUID varchar(255),
-    idShop int unsigned,
-    idUserCategory int  unsigned,
-    idAdmin int unsigned,
-    foreign key (idShop) references shop (id) on update cascade,
-    foreign key (idUserCategory) references userCategory (id) on update cascade,
-    foreign key (idAdmin) references `user` (id) on update cascade
-);
+--
+-- Table structure for table `promouse`
+--
 
-create table promoUse (
-	id int unsigned auto_increment primary key,
-    useDate date,
-    uniqueCode varchar(255),
-    wasUser boolean,
-    idPromo int unsigned,
-    idOwner int unsigned,
-    idUser int unsigned,
-    foreign key (idOwner) references `user` (id) on update cascade,
-	foreign key (idUser) references `user` (id) on update cascade,
-    foreign key (idPromo) references promotion (id) on update cascade
-);
+DROP TABLE IF EXISTS `promouse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promouse` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `useDate` date DEFAULT NULL,
+  `uniqueCode` varchar(255) DEFAULT NULL,
+  `wasUser` tinyint(1) DEFAULT NULL,
+  `idPromo` int unsigned DEFAULT NULL,
+  `idOwner` int unsigned DEFAULT NULL,
+  `idUser` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idOwner` (`idOwner`),
+  KEY `idUser` (`idUser`),
+  KEY `idPromo` (`idPromo`),
+  CONSTRAINT `promouse_ibfk_1` FOREIGN KEY (`idOwner`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `promouse_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `promouse_ibfk_3` FOREIGN KEY (`idPromo`) REFERENCES `promotion` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table validPromoDay (
-	id int unsigned primary key,
-    weekDay int unsigned,
-    idPromotion int unsigned,
-    foreign key (idPromotion) references promotion (id) on update cascade
-);
+--
+-- Table structure for table `shop`
+--
 
-create table shopImages (
-	id int unsigned auto_increment primary key,
-    imageUUID varchar(255),
-    idShop int unsigned,
-    foreign key (idShop) references shop (id) on update cascade
-);
+DROP TABLE IF EXISTS `shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shop` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `dateDeleted` date DEFAULT NULL,
+  `idOwner` int unsigned DEFAULT NULL,
+  `idShopType` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `idOwner` (`idOwner`),
+  KEY `idShopType` (`idShopType`),
+  CONSTRAINT `shop_ibfk_1` FOREIGN KEY (`idOwner`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `shop_ibfk_2` FOREIGN KEY (`idShopType`) REFERENCES `shoptype` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shopimages`
+--
+
+DROP TABLE IF EXISTS `shopimages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shopimages` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `imageUUID` varchar(255) DEFAULT NULL,
+  `idShop` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idShop` (`idShop`),
+  CONSTRAINT `shopimages_ibfk_1` FOREIGN KEY (`idShop`) REFERENCES `shop` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shoptype`
+--
+
+DROP TABLE IF EXISTS `shoptype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shoptype` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  `description` text,
+  `dateDeleted` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `type` (`type`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) DEFAULT NULL,
+  `isOwner` tinyint(1) DEFAULT NULL,
+  `dateDeleted` date DEFAULT NULL,
+  `emailToken` varchar(255) DEFAULT NULL,
+  `isEmailVerified` tinyint(1) DEFAULT NULL,
+  `idUserCategory` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `emailToken` (`emailToken`),
+  KEY `idUserCategory` (`idUserCategory`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idUserCategory`) REFERENCES `usercategory` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usercategory`
+--
+
+DROP TABLE IF EXISTS `usercategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usercategory` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `categoryType` varchar(255) NOT NULL,
+  `dateDeleted` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `categoryType` (`categoryType`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `validpromoday`
+--
+
+DROP TABLE IF EXISTS `validpromoday`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `validpromoday` (
+  `id` int unsigned NOT NULL,
+  `weekDay` int unsigned DEFAULT NULL,
+  `idPromotion` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idPromotion` (`idPromotion`),
+  CONSTRAINT `validpromoday_ibfk_1` FOREIGN KEY (`idPromotion`) REFERENCES `promotion` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-28 12:21:17
