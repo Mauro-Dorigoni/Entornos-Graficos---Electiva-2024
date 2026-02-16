84f7ca3131e334d79d8af28b239c5d270af70ef3
Lautaro498 <lautarobrancatti03@gmail.com>
2026-02-16 11:33:13 -0300

    boton al index desde el login

--- TPI-PromoShop/Frontend/pages/loginPage.php
+++ TPI-PromoShop/Frontend/pages/loginPage.php
@@ -5,6 +5,7 @@ include "../components/messageModal.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,48 +16,53 @@ include "../components/messageModal.php";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../assets/styles/loginPage.css">
 </head>
+
 <body>
     <section class="vh-100">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col col-xl-10">
-                <div class="card" style="border-radius: 1rem; background-color: #eae8e0">
-                    <div class="row g-0">
-                    <div class="col-md-6 col-lg-5 d-none d-md-flex justify-content-center align-items-center" style="background-color: white; border-radius: 1rem 0 0 1rem;">
-                        <img src="../assets/LogoPromoShopFondoBlanco.png"
-                            alt="login form" class="img-fluid" style="max-width: 80%; height: auto;" />
-                    </div>
-                        <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                        <div class="card-body p-4 p-lg-5 text-black">
-                            <form action=<?php echo backendHTTPLayer . '/login.http.php'; ?> method="post">
-                                <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Ingrese con su cuenta</h5>
+                <div class="col col-xl-10">
+                    <div class="card" style="border-radius: 1rem; background-color: #eae8e0">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-flex justify-content-center align-items-center" style="background-color: white; border-radius: 1rem 0 0 1rem;">
+                                <img src="../assets/LogoPromoShopFondoBlanco.png"
+                                    alt="login form" class="img-fluid" style="max-width: 80%; height: auto;" />
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5 text-black">
+                                    <form action=<?php echo backendHTTPLayer . '/login.http.php'; ?> method="post">
+                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Ingrese con su cuenta</h5>
 
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <input type="email" id="form2Example17" class="form-control form-control-lg" id="email" name="email" required/>
-                                    <label class="form-label" for="form2Example17">Direccion de email</label>
-                                </div>
+                                        <div data-mdb-input-init class="form-outline mb-4">
+                                            <input type="email" id="form2Example17" class="form-control form-control-lg" id="email" name="email" required />
+                                            <label class="form-label" for="form2Example17">Direccion de email</label>
+                                        </div>
 
-                                <div data-mdb-input-init class="form-outline mb-4">
-                                    <input type="password" id="form2Example27" class="form-control form-control-lg" id="pass" name="pass" required/>
-                                    <label class="form-label" for="form2Example27" >Contraseña</label>
-                                </div>
+                                        <div data-mdb-input-init class="form-outline mb-4">
+                                            <input type="password" id="form2Example27" class="form-control form-control-lg" id="pass" name="pass" required />
+                                            <label class="form-label" for="form2Example27">Contraseña</label>
+                                        </div>
 
-                                <div class="pt-1 mb-4">
-                                    <button type="submit" class="btn btn-lg btn-block btn-outline-orange">Ingresar</button>
+                                        <div class="pt-1 mb-1">
+                                            <button type="submit" class="btn btn-lg btn-block btn-outline-orange">Ingresar</button>
+                                        </div>
+                                        <div class="pt-1 mb-4">
+                                            <a href="index.php" class="btn btn-lg btn-block btn-secondary">Ingresar sin Logearse</a>
+                                        </div>
+                                        <a class="small text-muted" href=<?php echo frontendURL . "/passwordRecoveryPage.php" ?>>Olvido su Contraseña?</a>
+                                        <p class="mb-5 pb-lg-2" style="color: #393f81;">No tienes una cuenta? <a href=<?php echo frontendURL . "/registerPage.php" ?>
+                                                style="color: #393f81;">Registrate Aqui</a></p>
+                                        <a href=<?php echo frontendURL . "/termsPage.php" ?> class="small text-muted">Terminos de Uso.</a>
+                                        <a href=<?php echo frontendURL . "/privacyPage.php" ?> class="small text-muted">Politica de Privacidad</a>
+                                    </form>
                                 </div>
-                                <a class="small text-muted" href=<?php echo frontendURL."/passwordRecoveryPage.php"?>>Olvido su Contraseña?</a>
-                                <p class="mb-5 pb-lg-2" style="color: #393f81;">No tienes una cuenta? <a href=<?php echo frontendURL."/registerPage.php"?>
-                                    style="color: #393f81;">Registrate Aqui</a></p>
-                                <a href=<?php echo frontendURL."/termsPage.php"?> class="small text-muted">Terminos de Uso.</a>
-                                <a href=<?php echo frontendURL."/privacyPage.php"?> class="small text-muted">Politica de Privacidad</a>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
-            </div>
             </div>
         </div>
     </section>
 </body>
+
 </html>
