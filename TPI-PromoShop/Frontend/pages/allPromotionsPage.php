c12cc61e4e9563630350e2f324f7d47696187f58
Lautaro498 <lautarobrancatti03@gmail.com>
2026-03-08 18:38:14 -0300

    practica para final

--- TPI-PromoShop/Frontend/pages/allPromotionsPage.php
+++ TPI-PromoShop/Frontend/pages/allPromotionsPage.php
@@ -45,7 +45,7 @@ try {
 
 //LOGICA DE FILTRADO. SE HACE A NIVEL DE MEMORIA. POCO EFICIENTE. LAUTARO
 // Aplicar el filtro
-$promocionesFiltradas = array_filter(
+$promocionesFiltradasSinOrdenar = array_filter(
     $promotions,
     function (Promotion $promo) use ($f_shopType, $f_userCat, $search) {
         //  FILTRO: ShopType 
@@ -89,8 +89,15 @@ $promocionesFiltradas = array_filter(
 );
 
 // 3. Reordenar índices (Opcional pero recomendado para JSON o bucles simples)
-$promocionesFiltradas = array_values($promocionesFiltradas);
+$promocionesFiltradas = array_values($promocionesFiltradasSinOrdenar);
 
+$totalPromotions = count($promocionesFiltradas);
+
+// Cantidad por página
+$limit = 3;
+$totalPaginas = ceil($totalPromotions / $limit);
+
+$grupos = array_chunk($promocionesFiltradas, $limit);
 
 ?>
 
